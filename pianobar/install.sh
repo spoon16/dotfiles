694b72976e04f6dce1b238b67022fda07f01c870
Eric Schoonover <erics@capdes.com>
2011-09-16 20:58:15 -0700

    Added pianobar configuration, fixed up gitconfig

---
+++ pianobar/install.sh
@@ -0,0 +1,3 @@
+ln -s ~/src/dotfiles/pianobar/config ~/.config/pianobar/config
+ln -s ~/src/dotfiles/pianobar/pianobar-notify.rb ~/bin/pianobar-notify.rb
+ln -s ~/src/dotfiles/pianobar/pandora-icon.png ~/.config/pianobar/pandora-icon.png
